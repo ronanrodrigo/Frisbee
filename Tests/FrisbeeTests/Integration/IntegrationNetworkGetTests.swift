3d96893e77706efa38797f4c3e7097beb684aeee
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-12-18 09:47:39 -0200

    Add integration tests

---
+++ Tests/FrisbeeTests/Integration/IntegrationNetworkGetTests.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import Frisbee
+
+final class IntegrationNetworkGetTests: XCTestCase {
+
+    struct Movie: Decodable {
+        let name: String
+    }
+
+    func testGetWhenHasValidURLWithValidEntityThenRequestAndTransformData() {
+        let url = "https://gist.githubusercontent.com/ronanrodrigo" +
+                  "/fbb32cee20e43f0f9972c9a3230ef93d/raw/2cb87fd40e65fadb25fcc30f643d385bccdb5f7c/movie.json"
+        let longRunningExpectation = expectation(description: "RegisterUserWithAlreadyInUseEmail")
+        let expectedMovieName = "Ghostbusters"
+        var returnedData: Movie?
+
+        NetworkGetter().get(url: url) { (result: Result<Movie>) in
+            returnedData = result.data
+            longRunningExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 20) { expectationError in
+            XCTAssertNil(expectationError, expectationError!.localizedDescription)
+            XCTAssertEqual(returnedData?.name, expectedMovieName)
+        }
+    }
+
+}
