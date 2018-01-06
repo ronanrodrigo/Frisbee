5ee2430252d63e5fc9326d8e7648366189105a9e
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-01-06 10:31:02 -0200

    Update linux tests support

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,11 +2,11 @@ import XCTest
 @testable import FrisbeeTests
 
 XCTMain([
-    testCase(NetworkGetterTests.allTests)
-    testCase(IntegrationNetworkGetTests.allTests)
-    testCase(URLSessionFactoryTests.allTests)
-    testCase(URLRequestFactoryTests.allTests)
-    testCase(QueryItemBuilderTests.allTests)
-    testCase(URLWithQueryBuilderTests.allTests)
+    testCase(NetworkGetterTests.allTests),
+    testCase(IntegrationNetworkGetTests.allTests),
+    testCase(URLSessionFactoryTests.allTests),
+    testCase(URLRequestFactoryTests.allTests),
+    testCase(QueryItemBuilderTests.allTests),
+    testCase(URLWithQueryBuilderTests.allTests),
     testCase(ResultGeneratorTests.allTests)
 ])
