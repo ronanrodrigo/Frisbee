f6b79d0fcfc79d9fc1cc0c9cf826f810b3df89bd
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-01-16 12:22:08 -0200

    Update request URL in integration tests

--- Tests/FrisbeeTests/Requestables/IntegrationNetworkPostTests.swift
+++ Tests/FrisbeeTests/Requestables/IntegrationNetworkPostTests.swift
@@ -4,32 +4,36 @@ import Frisbee
 final class IntegrationNetworkPostTests: XCTestCase {
 
     struct Json: Codable {
-        let content: String
-        let url: String
+        let id: Int? // swiftlint:disable:this identifier_name
+        let content: String?
+        let url: String?
+    }
+
+    struct ReqresId: Codable {
+        let id: String // swiftlint:disable:this identifier_name
     }
 
     func testPostWhenHasValidURLThenRequestAndTransformData() {
-        let url = URL(string: "https://putsreq.com/RrJlwsma8cM8TjC1ipmB")!
+        let url = URL(string: "https://reqres.in/api/users")!
         let longRunningExpectation = expectation(description: "RequestMoviesWithSuccess")
-        var returnedData: Json?
+        var returnedData: ReqresId?
 
-        NetworkPost().post(url: url) { (result: Result<Json>) in
+        NetworkPost().post(url: url) { (result: Result<ReqresId>) in
             returnedData = result.data
             longRunningExpectation.fulfill()
         }
 
         waitForExpectations(timeout: 20) { expectationError in
             XCTAssertNil(expectationError, expectationError!.localizedDescription)
-            XCTAssertEqual(returnedData?.url, url.absoluteString)
-            XCTAssertEqual(returnedData?.content, "content")
+            XCTAssertNotNil(returnedData?.id)
         }
     }
 
     func testPostWhenHasValidURLAndWithBodyThenRequestAndTransformData() {
-        let url = URL(string: "https://putsreq.com/RrJlwsma8cM8TjC1ipmB")!
+        let url = URL(string: "https://reqres.in/api/users")!
         let longRunningExpectation = expectation(description: "RequestMoviesWithSuccess")
         var returnedData: Json?
-        let body = Json(content: #function, url: url.absoluteString)
+        let body = Json(id: 123, content: #function, url: url.absoluteString)
 
         NetworkPost().post(url: url, body: body) { (result: Result<Json>) in
             returnedData = result.data
@@ -38,6 +42,7 @@ final class IntegrationNetworkPostTests: XCTestCase {
 
         waitForExpectations(timeout: 20) { expectationError in
             XCTAssertNil(expectationError, expectationError!.localizedDescription)
+            XCTAssertEqual(returnedData?.id, body.id)
             XCTAssertEqual(returnedData?.url, body.url)
             XCTAssertEqual(returnedData?.content, body.content)
         }
